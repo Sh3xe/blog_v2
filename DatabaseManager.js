fd868bcbd31f41f65064a7146138b36167042801
Arnaud Pelissier <qwerlyz666@gmail.com>
2020-06-10 16:23:26 +0200

    Added functions to the database manager

--- DatabaseManager.js
+++ DatabaseManager.js
@@ -6,9 +6,42 @@ class DatabaseManager{
             host     : host,
             user     : user,
             password : password,
-            databse  : database
+            database  : database
         });
         this.connection.connect();
+    }
+
+    addArticle(title, content, user_id){
+        return new Promise((resolve, reject)=>{
+            if(!isNaN(parseInt(user_id))){
+                this.connection.query("INSERT INTO `blog_articles` (`article_title`, `article_content`, `article_user`) VALUES (?, ?, ?)", [title, content, user_id], (err, res, fld)=>{
+                    if(err) reject(err);
+                    resolve("sucessfully added!");
+                });
+            } else{
+                reject("Article_id is not a number");
+            }
+        });
+    }
+
+    getArticleById(id){
+        return new Promise((resolve, reject)=>{
+            if(!isNaN(parseInt(id))){
+                this.connection.query(`SELECT * FROM blog_articles WHERE article_id = ${id}`, (err, res, fld)=>{
+                    if (err){
+                        reject(err)
+                    } else{
+                        if(res.length != 0){
+                            resolve(res);
+                        } else{
+                            reject('Article not found');
+                        }
+                    }
+                });
+            } else{
+                reject("Article ID is not a number");
+            }
+        });
     }
 
     endConnection(){
--- app.js
+++ app.js
@@ -7,9 +7,25 @@ const {DatabaseManager} = require("./DatabaseManager.js");
 const config = require("./config.js");
 
 //Init database
-let database = new DatabaseManager(config.db_host, config.db_user, config.db_password, config.database);
+//let database = new DatabaseManager(config.db_host, config.db_user, config.db_password, config.db_database);
 
 //Init middlewares
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html.html"));
+});
+
+app.get("/poster", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/upload.html"));
+});
+
+app.get("/post/:id", (req, res) => {
+    console.log(req.params.id);
+    res.sendFile(path.join(__dirname, "public/upload.html"));
+});
+
+
+app.listen(config.app_port);
