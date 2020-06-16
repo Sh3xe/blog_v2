f0e6109c5f20c9aba45ef20d15db882fb371307c
Arnaud Pelissier <qwerlyz666@gmail.com>
2020-06-16 21:40:50 +0200

    Added more functionalities to the chat app, but code will need to be cleaned up

--- chat_app.js
+++ chat_app.js
@@ -3,6 +3,8 @@
 const utils = require("./utils.js");
 const fs = require("fs");
 
+// THIS CODE IS HORRIBLE AND WILL BE REWRITTEN
+
 class ChatApp {
     constructor(io_obj) {
         this.io = io_obj;
@@ -13,20 +15,41 @@ class ChatApp {
             const key = utils.getKeyFromCookie(socket.handshake.headers['cookie']);
             const user = this.findUser(key);
             if (user){
-                socket.user_id = user.id;
+                socket.user_id   = user.id;
                 socket.user_name = user.name;
                 this.socket_list.push(socket);
-                this.emitMessage(`${socket.user_name} has connected`, "SERVER", 0);
+
+                socket.emit("message_list", this.messages_list);
+                this.emitLog(`${socket.user_name} has connected`);
+                this.updateUserList();
             }
             
             socket.on("chat_message", (msg)=> {
+                this.messages_list.push({content: msg, user:socket.user_name, user_id: socket.user_id});
+                if(this.messages_list.length > 30){
+                    this.messages_list.shift();
+                }
+
                 this.emitMessage(msg, socket.user_name, socket.user_id);
             });
             
             socket.on("disconnect", ()=> {
-                this.emitMessage(`${socket.user_name} has disconnected`, "SERVER", 0);
+                this.socket_list = this.socket_list.filter(function(el) { return el != socket; });
+                this.emitLog(`${socket.user_name} has disconnected`);
             });
         });
+    }
+
+    updateUserList(){
+        let users = [];
+
+        for(let socket of this.socket_list){
+            users.push({name: socket.user_name, id:socket.user_id});
+        }
+
+        for(let socket of this.socket_list){
+            socket.emit("user_list_update", users);
+        }
     }
 
     emitMessage(msg, user_name, user_id) {
@@ -34,6 +57,12 @@ class ChatApp {
 
         for(let socket of this.socket_list){
             socket.emit("chat_message", {content: message, user: user_name,  user_id: user_id});
+        }
+    }
+
+    emitLog(msg){
+        for(let socket of this.socket_list){
+            socket.emit("server_message", msg);
         }
     }
 
--- public/js/client.js
+++ public/js/client.js
@@ -2,6 +2,7 @@ let socket = io();
 
 let msg_form = document.querySelector(".chat-form");
 let msg_container = document.querySelector(".chat-messages");
+let chat_aside = document.querySelector(".chat-aside ul");
 
 function currFormatedTime(){
     let d = new Date();
@@ -9,7 +10,11 @@ function currFormatedTime(){
 }
 
 function createMessage(msg){
-    return `<div class="message"><div class="msg-head"><a href="/user/${msg.user_id}">${msg.user}</a> le ${currFormatedTime()}</div><div class="msg-content">${msg.content}</div></div>`
+    return `<div class="message"><div class="msg-head"><a href="/user/${msg.user_id}">${msg.user}</a> le ${currFormatedTime()}</div><div class="msg-content">${msg.content}</div></div>`;
+}
+
+function createLogMessage(msg){
+    return `<div class="message"><div class="msg-head">SERVER le ${currFormatedTime()}</div><div class="msg-content">${msg}</div></div>`;
 }
 
 msg_form.addEventListener("submit", e=>{
@@ -21,4 +26,23 @@ msg_form.addEventListener("submit", e=>{
 socket.on("chat_message", (msg)=>{
     msg_container.innerHTML += createMessage(msg);
     msg_container.scrollTop = msg_container.scrollHeight;
+});
+
+socket.on("server_message", (msg)=>{
+    msg_container.innerHTML += createLogMessage(msg);
+    msg_container.scrollTop = msg_container.scrollHeight;
+});
+
+socket.on("message_list", (messages)=>{
+    for(let message of messages){
+        msg_container.innerHTML += createMessage(message);
+    }
+    msg_container.scrollTop = msg_container.scrollHeight;
+});
+
+socket.on("user_list_update", (users)=>{
+    chat_aside.innerHTML ="";
+    for(let user of users){
+        chat_aside.innerHTML += `<li><a href=${user.id}>${user.name}</a></li>`;
+    }
 });
